1beb4865a38bc852817d76cc4cf75a14aa844692
Charles Sears <charlesjsears@Charless-MBP.lan>
2022-09-16 14:12:14 -0400

    my first version

---
+++ house_painter.py
@@ -0,0 +1,31 @@
+numRooms = int(input('how many rooms are you planning to paint?:'))
+
+numWallsPerRoom = []
+i = 0
+for i in range(numRooms):
+    numWallsPerRoom[i] = i
+    #numWallsPerRoom[i] = int(input('how many walls are you painting:'))
+
+
+paintPerWall = input('how much paint do you need to paint a wall?:')
+
+totalPaint = 0
+for i in numRooms:
+    totalPaint += numWallsPerRoom[i]*paintPerWall
+
+print("total gallons of paint needed: ", totalPaint)
+
+totalCost = totalPaint * 25
+
+print("total cost:", totalCost)
+
+#maybe different colors
+#dont want to paint windows or doors
+#don't paint sockets
+#different sized windows
+#types of paint, different costs
+#create a calculaator for how much money it will cost
+#use everything we have covered
+#find total paint in gallons and price
+#ask for length and hieght of each room
+#find the most cost effective method
